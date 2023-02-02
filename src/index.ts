b027e6e1d1331e55e1212def925e9143a060f40b
M1chalS <michalszajner@wp.pl>
2023-02-02 22:49:25 +0100

    Connected to MongoDB cluster

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import express from "express";
 import {json} from 'body-parser';
+import mongoose from "mongoose";
+require("dotenv").config();
 
 const app = express();
 app.use(json());
+
+console.log(process.env.DB_USERNAME);
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.pfspadv.mongodb.net/?retryWrites=true&w=majority`)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(e => console.log(e.message));
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
